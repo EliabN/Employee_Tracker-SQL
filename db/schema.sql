0dfae7f5795361ba8513bd2795ec7bc64cfed3ca
eliabn <30064396@tafe.wa.edu.au>
2023-09-01 23:21:49 +0930

    Created tables in schema.sql file

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,27 @@
+DROP DATABASE IF EXIST employees_db;
+CREATE DATABASE employees_db;
+
+USE employees_db;
+
+CREATE TABLE department (
+    id INT AUTO INCREMENT NOT NULL,
+    department VARCHAR(30) NOT NULL
+    PRIMARY KEY (id)
+);
+
+
+CREATE TABLE role (
+    id INT AUTO INCREMENT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
 
 
 
 
 
+ 

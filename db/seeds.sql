9f8d99f1d0057b88c3d52460f46441e4730704ad
eliabn <30064396@tafe.wa.edu.au>
2023-09-02 19:16:43 +0930

    Insert function for employee in seeds.sql file

--- db/seeds.sql
+++ db/seeds.sql
@@ -15,3 +15,14 @@ VALUES  (1, "Sales Lead", 100000),
         (3,	"Accountant" 125000),
         (2,	"Legal Team Lead", 250000),
         (2,	"Lawyer", 190000);
+
+
+INSERT INTO employee (role_id, first_name, last_name)
+VALUES  (1, "John", "Doe"),
+        (2,	"Mike", "Chan"),
+        (3,	"Ashley", "Rodriguez"),
+        (4,	"Kevin", "Tupik"),
+        (5,	"Kunal", "Singh"),
+        (6,	"Malia", "Brown"),
+        (7,	"Sarah", "Lourd"),
+        (8,	"Tom",	"Allen");

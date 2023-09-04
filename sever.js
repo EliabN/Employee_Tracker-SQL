cb5972f8d38d3290eaf64f4581242cf96085ec00
eliabn <30064396@tafe.wa.edu.au>
2023-09-04 14:55:12 +0930

    Create a function to connect to mysql

--- sever.js
+++ sever.js
@@ -5,6 +5,31 @@ const mysql = require('mysql2');
 const inquirer = require('inquirer');
 
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      // MySQL password
+      password: '',
+      database: 'company_db'
+    },
+    console.log(`Connected to the books_db database.`)
+);
+
+
+// Create an array of options for user input
+const options = [
+    {
+      type: 'list',
+      name: 'Main',
+      message: 'What would do uou like to do:',
+      choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role'],
+    },
+]
+
+
 // Create a function to initialize/CLI app
 function init() {
     inquirer

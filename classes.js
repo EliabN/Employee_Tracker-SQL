d8672ef1633c63b2bbaec377d9be153cf8a0a77a
eliabn <30064396@tafe.wa.edu.au>
2023-09-04 17:21:54 +0930

    Create classes.js and Department class

---
+++ classes.js
@@ -0,0 +1,11 @@
+// Create class for instance object of Department table
+class Department {
+    constructor(id, name) {
+      this.id = id;
+      this.name = name;
+    }
+}
+
+
+// Export class
+module.export = Department;
